c3253ce1ca73b68c856becc37962723c338a1427
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2024-02-01 10:05:54 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio !== undefined && props.bio !==''? <p>{props.bio}</p>: null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <div>
+        <h2>Links</h2>
+        <a href={props.github}>{props.github}</a>
+        <a href={props.linkedin}>{props.linkedin}</a>
+      </div>
     </div>
   );
 }
